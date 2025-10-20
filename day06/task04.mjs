dea0c328befb51a0a00edaee10d5135ad18216c0
Louis <louisavon77@gmail.com>
2025-10-20 15:55:41 +0200

    day06

--- day06/task04.mjs
+++ day06/task04.mjs
@@ -1,7 +1,15 @@
-export function fizzBuzz(number){
-   if number %3;
-    print ("Fizz")
-    %5;
-    print ("Buzz")
-    else print ("FizzBuzz")
+export function fizzBuzz(n) {
+    const resultats = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            resultats.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            resultats.push("Fizz");
+        } else if (i % 5 === 0) {
+            resultats.push("Buzz");
+        } else {
+            resultats.push(i);
+        }
+    }
+  console.log(resultats.join(", "));
 }
